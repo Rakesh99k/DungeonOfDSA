6a77aa6e6ee4fba47ed995ee074516b48ed8129d
Rakesh <124860454+Rakesh99k@users.noreply.github.com>
2025-09-03 23:50:52 +0530

    CountDigits

---
+++ src/Basic_Maths/CountDigits.java
@@ -0,0 +1,17 @@
+package Basic_Maths;
+
+import java.util.Scanner;
+
+public class CountDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int num = sc.nextInt();
+        int count = 0;
+        while(num > 0){
+            count = count + 1;
+            num = num/10;
+        }
+        System.out.println("The number of digits is : "+count);
+    }
+}
